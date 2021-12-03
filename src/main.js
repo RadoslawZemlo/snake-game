d61b4ee274854f1d024e570b326ef1e18333a759
RadoslawZemlo <radoslawzemlo1@gmail.com>
2021-12-03 16:36:28 +0100

    Add mainLoop and drawGame functions

--- src/main.js
+++ src/main.js
@@ -9,6 +9,52 @@ const random = max => {
   return Math.floor(Math.random() * (max + 1));
 };
 
+const drawGame = () => {
+  const [foodColor, snakeColor, stroke] = ["#c80e00", "#ffffff", "#555555"];
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // DRAW FOOD
+  ctx.fillStyle = foodColor;
+  ctx.fillRect(food.x * tileSize, food.y * tileSize, tileSize, tileSize);
+
+  ctx.strokeStyle = stroke;
+  ctx.strokeRect(food.x * tileSize, food.y * tileSize, tileSize, tileSize);
+
+  // DRAW SNAKE
+  snake.forEach(part => {
+    ctx.fillStyle = snakeColor;
+    ctx.fillRect(part.x * tileSize, part.y * tileSize, tileSize, tileSize);
+
+    ctx.strokeStyle = stroke;
+    ctx.strokeRect(part.x * tileSize, part.y * tileSize, tileSize, tileSize);
+  });
+};
+
+const mainLoop = () => {
+  const head = {
+    x: snake[0].x + dx,
+    y: snake[0].y + dy
+  };
+
+  snake.unshift(head);
+
+  if (head.x === food.x && head.y === food.y) {
+    score++;
+
+    food = {
+      x: random(gridSize - 1),
+      y: random(gridSize - 1)
+    };
+  } else {
+    snake.pop();
+  }
+
+  drawGame();
+
+  setTimeout(mainLoop, 120);
+};
+
 const newGame = () => {
   tileSize = canvas.width / gridSize;
 
@@ -31,4 +77,4 @@ const newGame = () => {
   mainLoop();
 };
 
-const mainLoop = () => {};
+newGame();
