503da3acaf32785f4c96fb28876bfe405a7f9244
RadoslawZemlo <radoslawzemlo1@gmail.com>
2021-12-03 16:47:40 +0100

    Add control snake function and hitting in wall mechanism

--- src/main.js
+++ src/main.js
@@ -3,7 +3,29 @@ const ctx = canvas.getContext("2d");
 
 const gridSize = 20;
 
-let tileSize, food, snake, dx, dy, score;
+let tileSize, food, snake, dx, dy, turning, score;
+
+const turn = key => {
+  if (turning) return;
+  turning = true;
+
+  const [movingLeft, movingUp, movingRigth, movingDown] = [
+    dx === -1,
+    dy === -1,
+    dx === 1,
+    dy === 1
+  ];
+
+  if ((key === "ArrowLeft" || key === "a") && !movingRigth) {
+    [dx, dy] = [-1, 0];
+  } else if ((key === "ArrowUp" || key === "w") && !movingDown) {
+    [dx, dy] = [0, -1];
+  } else if ((key === "ArrowRight" || key === "d") && !movingLeft) {
+    [dx, dy] = [1, 0];
+  } else if ((key === "ArrowDown" || key === "s") && !movingUp) {
+    [dx, dy] = [0, 1];
+  }
+};
 
 const random = max => {
   return Math.floor(Math.random() * (max + 1));
@@ -36,6 +58,16 @@ const mainLoop = () => {
     x: snake[0].x + dx,
     y: snake[0].y + dy
   };
+
+  turning = false;
+
+  if (
+    head.x < 0 ||
+    head.x > gridSize - 1 ||
+    head.y < 0 ||
+    head.y > gridSize - 1
+  )
+    return;
 
   snake.unshift(head);
 
@@ -73,6 +105,11 @@ const newGame = () => {
   score = 0;
 
   canvas.focus();
+
+  canvas.addEventListener("keydown", e => {
+    if (e.defaultPrevented) return;
+    turn(e.key);
+  });
 
   mainLoop();
 };
